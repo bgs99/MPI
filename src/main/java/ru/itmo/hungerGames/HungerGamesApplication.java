295bb1a3bb703c6d090caf48359c6722472cc4cb
Bogdan Guliaev <bgs99c@gmail.com>
2022-05-17 21:34:52 +0300

    Empty Spring Boot project

---
+++ src/main/java/ru/itmo/hungerGames/HungerGamesApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.hungerGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HungerGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HungerGamesApplication.class, args);
+	}
+
+}
