d80775375ea0b7261714a7fcd6e7409d0a8fdc66
Bogdan Guliaev <bgs99c@gmail.com>
2022-05-31 01:18:38 +0300

    Fixed

--- src/main/webapp/src/app/components/sponsoring/tributes/tributes.component.ts
+++ src/main/webapp/src/app/components/sponsoring/tributes/tributes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { NavigationExtras, Router } from '@angular/router';
 import { Tribute } from 'src/app/models/tribute';
 import { TributesService } from 'src/app/services/tributes.service';
 
@@ -12,7 +13,13 @@ export class TributesComponent implements OnInit {
   tributesColumns: string[] = ['name', 'district', 'select'];
   tributes = new MatTableDataSource<Tribute>([]);
 
-  constructor(private tributesService: TributesService) { }
+  constructor(private router: Router, private tributesService: TributesService) { }
+
+  select(tribute: Tribute): void {
+    console.log('Passing tribute in state')
+    const navigationExtras: NavigationExtras = { state: { tribute } };
+    this.router.navigate(['/sponsoring/resources'], navigationExtras);
+  }
 
   ngOnInit(): void {
     this.tributesService.getTributes(0)
