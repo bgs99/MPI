a928e7413dbfa09df964290b8f98810c2af0ece4
s666p <s666p@gmail.com>
2022-06-01 01:45:53 +0300

    expand data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,6 +14,38 @@ VALUES (4, 'Tribute-4', 4);
 INSERT INTO tribute(id, name, district)
 VALUES (5, 'Tribute-5', 5);
 
+--Sponsors:
+INSERT INTO sponsor(id, name)
+VALUES (1, 'Sponsor-1');
+
+INSERT INTO sponsor(id, name)
+VALUES (2, 'Sponsor-2');
+
+INSERT INTO sponsor(id, name)
+VALUES (3, 'Sponsor-3');
+
+INSERT INTO sponsor(id, name)
+VALUES (4, 'Sponsor-4');
+
+INSERT INTO sponsor(id, name)
+VALUES (5, 'Sponsor-5');
+
+--Mentors:
+INSERT INTO mentor(id, name)
+VALUES (1, 'Mentor-1');
+
+INSERT INTO mentor(id, name)
+VALUES (2, 'Mentor-2');
+
+INSERT INTO mentor(id, name)
+VALUES (3, 'Mentor-3');
+
+INSERT INTO mentor(id, name)
+VALUES (4, 'Mentor-4');
+
+INSERT INTO mentor(id, name)
+VALUES (5, 'Mentor-5');
+
 --Resources:
 INSERT INTO resource(id, name, price)
 VALUES (1, 'Resource-1', 50);
