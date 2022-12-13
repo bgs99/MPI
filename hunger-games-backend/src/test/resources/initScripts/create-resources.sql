2c25447e49523a945c4517e0be09e1df10cd494a
hakimovigorv <hakimovigorv@gmail.com>
2022-12-13 11:32:34 +0300

    Fix getAllResources

--- hunger-games-backend/src/test/resources/initScripts/create-resources.sql
+++ hunger-games-backend/src/test/resources/initScripts/create-resources.sql
@@ -1,4 +1,3 @@
-INSERT INTO resources (resource_id, name, price)
-VALUES ('33ff5ee9-c0d7-4955-b2cd-a0aa3d484b98', 'Resource-test-1', 50);
-INSERT INTO resources (resource_id, name, price)
-VALUES ('47f75e81-4f14-4af5-bce2-b6d5af372d94', 'Resource-test-2', 100);
+INSERT INTO resources (resource_id, name, price) VALUES ('33ff5ee9-c0d7-4955-b2cd-a0aa3d484b98', 'Resource-test-1', 50);
+INSERT INTO resources (resource_id, name, price) VALUES ('47f75e81-4f14-4af5-bce2-b6d5af372d94', 'Resource-test-2', 100);
+INSERT INTO resources (resource_id, name, price) VALUES ('45a95e19-e0e9-460e-9cba-bcd275e717a3', 'Resource-test-3', 200);
