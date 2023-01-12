43a3e8ffd764a4b73849dcb90f701cf95a43beaf
Bogdan Guliaev <bgs99c@gmail.com>
2023-01-12 14:44:13 +0300

    Moderator review page selenium tests

--- hunger-games-backend/src/main/java/ru/itmo/hungergames/model/request/ApproveAdvertisingTextRequest.java
+++ hunger-games-backend/src/main/java/ru/itmo/hungergames/model/request/ApproveAdvertisingTextRequest.java
@@ -1,6 +1,7 @@
 package ru.itmo.hungergames.model.request;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.util.UUID;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class ApproveAdvertisingTextRequest {
     private UUID advertisingTextId;
     private boolean approved;
