f6356ec16da23135ab5c29492e3eec0088457a60
Bogdan Guliaev <bgs99c@gmail.com>
2023-01-17 11:34:36 +0300

    Fix sponsor drop script

--- hunger-games-backend/src/test/resources/initScripts/drop-sponsor.sql
+++ hunger-games-backend/src/test/resources/initScripts/drop-sponsor.sql
@@ -1,3 +1,4 @@
 delete from sponsors where user_id = '4a9f1d37-c6fd-4391-8082-655bb98fb460';
 delete from user_roles where user_id = '4a9f1d37-c6fd-4391-8082-655bb98fb460';
 delete from users where user_id = '4a9f1d37-c6fd-4391-8082-655bb98fb460';
+delete from settings where setting_id = '4a9f1d37-c6fd-4391-8082-655bb98fb460';
