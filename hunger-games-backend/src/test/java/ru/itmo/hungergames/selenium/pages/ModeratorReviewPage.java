e369722053526522ec16963d18d077e7157f1f67
Bogdan Guliaev <bgs99c@gmail.com>
2023-01-18 10:52:53 +0300

    Fix moderator review tests

--- hunger-games-backend/src/test/java/ru/itmo/hungergames/selenium/pages/ModeratorReviewPage.java
+++ hunger-games-backend/src/test/java/ru/itmo/hungergames/selenium/pages/ModeratorReviewPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class ModeratorReviewPage {
-    @FindBy(xpath = "//h2")
+    @FindBy(xpath = "//h2[contains(@class, 'status')]")
     private WebElement statusElement;
 
     public String getStatus() {
